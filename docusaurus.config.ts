115a728ea9b1baaf968fed9261fd6f79cb8b34e8
MariaMohsin <143216809+MariaMohsin@users.noreply.github.com>
2025-12-19 20:54:21 +0500

    Fix HeroSection, update Node version, remove rag_baknd from Git tracking

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://ai-native-book.vercel.app',
+  url: 'https://hackathon-book-topaz.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
